59058719898bb0ab75e9a6f769b209584505305f
aristokratos <oluwatobiloba.onawale@decagon.dev>
2023-02-28 14:12:34 +0100

    Clean Architecture .NET 6.0

---
+++ CleanArchitecture.Api/Helpers/Extension.cs
@@ -0,0 +1,129 @@
+﻿using System.Text;
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.Interfaces.Repositories;
+using CleanArchitecture.Repository;
+using CleanArchitecture.Repository.Base;
+using CleanArchitecture.Repository.DatabaseContext;
+using CleanArchitecture.Service;
+using DotNetCore.IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+namespace CleanArchitecture.Api.Helpers;
+
+public static class Extension
+{
+    public static void RegisterSeriLog()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("serilogconfig.json")
+            .Build();
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(config)
+            .CreateLogger();
+    }
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    public static void RegisterSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(option =>
+        {
+            option.SwaggerDoc("v1",
+                new OpenApiInfo
+                { Title = "CleanArchitecture.Net6", Version = "v1", Description = "Onion Architecture Using .Net6" });
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+            });
+        });
+    }
+
+    public static void AddCorsPolicy(this IServiceCollection services)
+    {
+        services.AddCors(option =>
+            option.AddPolicy("AppPolicy", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
+    }
+
+    public static void AddDbContext(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(services.GetConnectionString("DefaultConnection"));
+        });
+    }
+    public static void RegisterIdentity(this IServiceCollection services)
+    {
+        services.AddIdentityCore<IdentityUser>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
+            .AddRoles<IdentityRole>()
+            .AddRoleManager<RoleManager<IdentityRole>>()
+            .AddSignInManager<SignInManager<IdentityUser>>()
+            .AddRoleValidator<RoleValidator<IdentityRole>>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+    }
+    public static void RegisterAuthentication(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"])),
+                    ValidateIssuer = false,
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    ValidateAudience = false,
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                };
+            });
+    }
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IWeatherForecastService, WeatherForecastService>();
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
+    }
+
+    public static void RegisterAutoMapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(AppDomain.CurrentDomain
+            .GetAssemblies()
+            .Where(x => x.FullName!.StartsWith(nameof(CleanArchitecture))));    // ToDo: Change "CleanArchitecture" to "YOUR_PROJECT_BASE_NAMESPACE"
+    }
+}
---
+++ CleanArchitecture.Api/Program.cs
@@ -0,0 +1,50 @@
+using CleanArchitecture.Api.Helpers;
+using CleanArchitecture.Api.Middleware;
+using CleanArchitecture.Core.Converters;
+using CleanArchitecture.Core.Settings;
+using CleanArchitecture.Repository.DatabaseContext;
+using DotNetCore.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(options => options.ModelBinderProviders.Insert(0, new CustomModelBinderProvider()))
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new TrimStringConverter());
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.RegisterSwagger();
+builder.Host.UseSerilog();
+Extension.RegisterSeriLog();
+builder.Services.AddCorsPolicy();
+builder.Services.AddDbContext();
+builder.Services.RegisterIdentity();
+builder.RegisterAuthentication();
+builder.Services.AddServices();
+builder.Services.AddRepositories();
+builder.Services.RegisterAutoMapper();
+builder.Services.AddResponseCompression();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AppPolicy");
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseResponseCompression();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.MapControllers();
+
+app.Run();
